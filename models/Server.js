cda553c31dc51946bca514c8b6bf4941042629a7
Santiago Ochoa <santi_shy@hotmail.com>
2025-07-17 19:04:44 -0600

    aumentar limites de peso al fileupload

--- models/Server.js
+++ models/Server.js
@@ -47,7 +47,7 @@ class Server {
         useTempFiles: true,
         tempFileDir: "/tmp/",
         createParentPath: true,
-        limits: { fileSize: 20 * 1024 * 1024 } // 20 MB
+        limits: { fileSize: 50 * 1024 * 1024 } // 50 MB
       })
     );
   }
